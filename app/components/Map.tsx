1c1a6e3a4af81ebfe83f281a07b9d463f1bba011
fern-shy <stefanjp@bu.edu>
2025-03-03 14:02:57 -0500

    map edit

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -1,17 +1,14 @@
 "use client";
-// The 'use client' directive is necessary since this component uses browser-specific APIs and hooks.
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import React from "react";
 import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
-// Define the container styles for the map
 const containerStyle = {
   width: "100%",
   height: "400px",
 };
 
-// Set the initial center point (Boston, in this case)
+// Set the initial center point
 const center = {
   lat: 42.3601,
   lng: -71.0589,
